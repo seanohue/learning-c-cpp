7d0caf3eba0caef48803b656755321560db717f9
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-02-11 15:39:54 -0500

    Week 3, homework problem one is donezo.

---
+++ week-3-hw/findstring.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int find_string (char search_string[], char sub_string[]) {
+  char *p1, *p2, *p3;
+
+  p1 = search_string;
+  p2 = sub_string;
+
+  int i, j, found = 0;
+
+  for (i = 0 ; i < strlen(search_string); i ++) {
+    if (*p1 == *p2) {
+      p3 = p1;
+
+      for (j = 0 ; j < strlen(sub_string); j ++) {
+        if (*p3 == *p2) {
+          p3++;
+          p2++;
+        } else {
+          break;
+        }
+      }
+
+      p2 = sub_string;
+      if (j == strlen(sub_string)) {
+        found = 1;
+        return i;
+      }
+
+    }
+
+    p1++;
+  }
+  return -1;
+}
+
+int main (void) {
+  printf("Searching for captain in string potato captain:\n");
+  printf("found at index: %i", find_string("potato captain", "captain"));
+  return 0;
+}
